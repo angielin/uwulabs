1b3d1f534eeca757119df14b7f6d5979edf6dd8f
Angie Lin <angielin@Angies-MacBook-Pro.local>
2022-06-17 00:52:28 -0700

    Refactor template code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import EmotionsList from "./EmotionsList";
 
 function App() {
+  const [emotions, setEmotions] = useState(["Todo1", "Todo2"]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <EmotionsList emotions={emotions} />
+      <h1>In one word, tell me how you're feeling today?</h1>
+      <input type="test" />
+      <button> Submit </button>
+    </>
   );
 }
 
